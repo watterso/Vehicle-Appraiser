4c801d537b5782c63306ef3608bd118462aafd20
James Watterson <jameslwatterson@gmail.com>
2013-09-21 17:57:18 -0400

    mongo get requests work

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function test(){
-	var a  = 1+1;
-}
+$("[itemscope]").filter("tr").each(function (){
+	var tmpObj = {};
+	var cols = $(this).children();
+	var superTitle = cols.filter('.details').children().filter('.supertitle').text();
+	var titleSplit = superTitle.split(":");
+	tmpObj.make = titleSplit[0].trim();
+	var modelArr = titleSplit[1].trim().split(" ");
+	tmpObj.model = modelArr[0];
+	tmpObj.modelDetails = modelArr.join(" ");
+	tmpObj.year = parseInt(cols.filter('.year').first().text(),10);
+	var res = "<div class=\"appraise\">"+JSON.stringify(tmpObj)+"</div>";
+	$(res).appendTo($(this));
+	var mUrl = "https://api.mongolab.com/api/1/databases/uvclookup/collections/uvcdata?apiKey=jIHkv3sxi0kID-9TRpmg_5Yckd2dKF7M";
+	var query = {
+		"make": {"$regex":tmpObj.make, "$options":"i"},
+		"model": {"$regex":".*"+tmpObj.model+".*", "$options":"i"},
+		"year" : tmpObj.year
+	};
+	mUrl+="&q="+encodeURI(JSON.stringify(query));
+	$.get(mUrl, function (data){
+		console.log(JSON.stringify(data));
+	});
+});
